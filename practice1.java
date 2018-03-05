7f5f8bee2a2ff733cd196dbf24bce1127803a653
b10402113 <37054745+b10402113@users.noreply.github.com>
2018-03-05 10:15:39 +0800

    practice1
    asdas

---
+++ practice1.java
@@ -0,0 +1,12 @@
+public class practice1
+{
+	public static void main(String[] args)
+	{
+		//char 只能用單引號
+		char ch = '\n';
+		String num1 = "one" + ch +"two";
+		int a = 7 % 2;
+		System.out.println(num1);
+		System.out.println(a);
+	}
+}
